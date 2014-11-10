55242b6db2737e4bf47b4624979b318b93037d9f
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-11-10 17:21:49 +0100

    Implementation du reste du générateur d'images

--- galerie-shell.sh
+++ galerie-shell.sh
@@ -2,8 +2,9 @@
 
 src=''
 dest=''
+title="Galerie d'images"
 verbose=0
-dry_run=0
+dry_run='' # dry_run inactif par defaut
 main_file='index.html'
 
 DIR=$(cd "$(dirname "$0")" && pwd)
@@ -27,6 +28,7 @@ utilisation: generate-img-fragment.sh [OPTIONS] SOURCE DEST
                                      \ta miniaturiser
              -d,  --dest REP         \tRepertoire cible pour vignettes 
                                      \tet fichier html                    
+             -t,  --title TITRE      \tTitre de la page
 EOF
 }
 
@@ -38,40 +40,31 @@ while test $# -ne 0; do
             exit 0
             ;;
         "--src" | "-s")
-            shift; src="$1"
+            shift; src="${1%/}"
             ;;
         "--dest" | "-d")
-            shift; dest="$1"
+            shift; dest="${1%/}"
             ;;
         "--main-file" | "-mf")
             shift; main_file="$1"
             ;;
+        "--title" | "-t")
+            shift; title="$1"
+            ;;
         "--dry-run" | "-dr")
-            dry_run=1
+            dry_run="echo"
             ;;
         "--verbose" | "-v")
             verbose=1
             ;;
         *)
-            echo "Argument non reconnu : $1"
+            echo "erreur: Argument non reconnu : $1"
             usage
             exit 1
             ;;
     esac
     shift
 done
-
-# Ouverture du fichier de sortie sur le desc 5
-if ! [ -d "$dest" ]; then
-    mkdir "$dest"
-fi
-exec 5>"$dest/$main_file"
-
-# On doit avoir au minimum le repertoire source et dest comme arguments
-if [ "$src" = "" -o "$dest" = "" ]; then
-    echo "Il manque un ou plusieurs arguments."
-    usage; exit 1
-fi
 
 # Si la verbose est inactive, redirection de nos sorties vers /dev/null
 if [ "$verbose" = 1 ]; then
@@ -80,25 +73,90 @@ else
     exec 4>/dev/null 3>/dev/null
 fi
 
-html_head "GALERIE DE CHATONS MIGNONS" >&5
-html_title "GALERIE DE CHATONS MIGNONS" >&5
+# On doit avoir au minimum le repertoire source et dest comme arguments
+if [ "$src" = "" -o "$dest" = "" ]; then
+    echo "erreur: Il manque un ou plusieurs arguments."
+    usage; exit 1
+fi
+
+if [ $dry_run ]; then 
+    printf "!-- Execution a sec, aucune commande ne sera executee --!\n\n"
+fi
+
+# Creation de l'arborescence du site si inexistante
+if ! [ -d "$dest" ]; then
+    $dry_run mkdir "$dest"
+fi
+if ! [ -d "$dest/images/" ]; then
+    $dry_run mkdir "$dest/images/"
+fi
+if ! [ -d "$dest/vignettes/" ]; then
+    $dry_run mkdir "$dest/vignettes/"
+fi
+
+## Ecriture du fichier HTML
+# Ouverture du fichier de sortie sur le desc 5
+# et ecriture du header et titre
+if [ $dry_run ]; then 
+    echo 'exec 5>' "$dest/$main_file"
+    echo 'html_head '"$title"' >&5'
+    echo 'html_title '"$title"' >&5'
+else 
+    exec 5> "$dest/$main_file"
+    html_head "$title" >&5
+    html_title "$title" >&5
+fi
 
 # Generation des vignettes pour chaque image jpeg de src
+printf "\n* Generation des vignettes... \n"
+
 for file in "$src"/*; do
-    filename=$(basename $file)
-    if [ -f "$dest/thumb-$filename" ]; then
-	echo "$dest/thumb-$filename existe deja, on l'ignore..." >&3
+    filename=$(basename "$file")
+
+    # Copie de l'image dans $dest/images/ si inexistante
+    if ! [ -f "$dest/images/$filename" ]; then
+	echo "cp $file $dest/images" >&3
+	$dry_run cp "$file" "$dest"/images/
+    fi
+    
+    # Si la vignette n'existe pas, la creer
+    if [ -f "$dest/vignettes/vg-$filename" ]; then
+        echo "$dest/vignettes/vg-$filename existe deja, on l'ignore..." >&3
     else 
-	echo "convert -thumbnail 320x240 $file $dest/thumb-$filename" >&3
-	convert -thumbnail 320x240 "$file" "$dest/thumb-$filename"
-	echo "generate-img-fragment.sh thumb-$filename alt-name legend >&5" >&3
-	cd "$dest"
-	"$DIR"/generate-img-fragment.sh "thumb-$filename" "alt-name" "legend" >&5
-	cd "$DIR"
+	echo "convert -thumbnail 320x240 $file $dest/vignettes/vg-$filename" >&3
+	$dry_run convert -thumbnail 320x240 \
+	    "$file" "$dest/vignettes/vg-$filename"
     fi
 done
 
-html_tail >&5
+# Ecriture des images dans le fichier HTMl
+printf "\n* Ecriture des images dans le fichier HTMl...\n"
 
-printf "\nLe fichier html genere est disponible ici: $dest/$main_file\n"
+echo "cd $dest" >&3
+$dry_run cd "$dest"
+
+for img in images/*; do
+    imageName=$(basename "$img")
+    
+    echo "--> Veuillez entrer la legende de l'image $imageName: "
+    $dry_run read legend
+	
+    printf "generate-img-fragment.sh vignettes/vg-$imageName" \
+	"images/$imageName " >&3
+    printf "$imageName $legend > $main_file \n" >&3
+
+    if [ $dry_run ]; then 
+	echo "$DIR"/generate-img-fragment.sh vignettes/vg-"$imageName" \
+	    images/"$imageName" "$imageName" "$legend"' >&5'
+    else
+	"$DIR"/generate-img-fragment.sh vignettes/vg-"$imageName" \
+	    images/"$imageName" "$imageName" "$legend" >&5
+    fi
+done
+
+$dry_run cd "$DIR"
+
+if [ $dry_run ]; then echo 'html_tail >&5'; else html_tail >&5; fi
+
+printf "\n* Fini: Le fichier html genere est disponible ici: $dest/$main_file\n"
 exit 0
--- generate-img-fragment.sh
+++ generate-img-fragment.sh
@@ -1,21 +1,28 @@
 #! /bin/sh
 
 usage () {
-    echo 'usage: generate-img-fragment.sh src "alt-name" "legend"'
+    echo 'usage: generate-img-fragment.sh vignette image ' \
+	'"alt-name" "legend"' >&2
 }
 
-if [ $# -eq 3 ]; then
+if [ $# -eq 4 ]; then
     if ! [ -r $1 ]; then
-	echo "Le fichier $1 n'existe pas." >&2
+	echo "generate-img-fragment.sh: Le fichier $1 n'existe pas." >&2
+	exit 1;
+    elif ! [ -r $2 ]; then
+	echo "generate-img-fragment.sh: Le fichier $2 n'existe pas." >&2
+	exit 1;
     else
 	echo '<div class="imgframe">'
-	echo '<img class="image" src="'"$1"'" alt="'"$2"'"><br>'
-	echo '<span class="legend">'"$3"'</span>'
+	echo '<a href='"$2"' target="_blank">'
+	echo '<img class="image" src="'"$1"'" alt="'"$3"'"><br>'
+	echo '</a>'
+	echo '<span class="legend">'"$4"'</span>'
 	echo '</div>'
-	exit 0
     fi
-
+    
+    exit 0
 else
-    echo "Nombre d'arguments invalide." >&2
+    echo "generate-img-fragment.sh: Nombre d'arguments invalide." >&2
     usage; exit 1;
 fi
