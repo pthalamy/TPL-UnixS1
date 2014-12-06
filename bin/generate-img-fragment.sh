2daae50c85073ae224fcbacbb004feb9616c38d3
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-12-06 13:30:24 +0100

    -legend -al, only 1 arg necessary

--- bin/generate-img-fragment.sh
+++ bin/generate-img-fragment.sh
@@ -4,23 +4,18 @@ alt="$3"
 legend="$4"
 
 usage () {
-    echo 'usage: generate-img-fragment.sh vignette image ' \
-	'"alt-name" "legend"' >&2
+    echo 'usage: generate-img-fragment.sh vignette viewerName' >&2
 }
 
-if [ $# -ge 2 ]; then
+if [ $# -ge 1 ]; then
     if ! [ -r $1 ]; then
 	echo "generate-img-fragment.sh: Le fichier $1 n'existe pas." >&2
 	exit 1;
-    elif ! [ -r $2 ]; then
-	echo "generate-img-fragment.sh: Le fichier $2 n'existe pas." >&2
-	exit 1;
     else
-	echo '<div class="imgframe">'
-	echo '<a href='"$2"' target="_blank">'
-	echo '<img class="image" src="'"$1"'" alt="'"$alt"'"><br>'
+	echo '<div class="vgframe">'
+	echo '<a href='"viewers/$2"' target="_top">'
+	echo '<img class="image" src='"$1"'><br>'
 	echo '</a>'
-	echo '<span class="legend">'"$legend"'</span>'
 	echo '</div>'
     fi
     
