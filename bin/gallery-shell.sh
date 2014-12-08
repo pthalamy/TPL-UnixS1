0c8ff1349065b7e66220ce12c8c73bee44964d5e
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-12-08 19:57:40 +0100

    Factorisation, implementation exif

--- bin/gallery-shell.sh
+++ bin/gallery-shell.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
 
+# Memorisation du repertoire de travail et includes
+DIR=$(cd "$(dirname "$0")" && pwd)
+. "$DIR"/utilities.sh
+
+# Variables globales
 src=''
 dest=''
 title="Galerie d'images"
 verbose=0
 dry_run='' # dry_run inactif par defaut
-main_file='index.html'
-
-DIR=$(cd "$(dirname "$0")" && pwd)
-. "$DIR"/utilities.sh
+main_file='index.html' 
 
 usage () {
     cat <<EOF
@@ -33,161 +35,187 @@ EOF
 }
 
 # Parsing des arguments du script
-while test $# -ne 0; do
-    case "$1" in
-        "--help"|"-h")
-            help
-            exit 0
-            ;;
-        "--source" | "--src" | "-s")
-            shift; src="${1%/}"
-            ;;
-        "--destination" | "--dest" | "-d")
-            shift; dest="${1%/}"
-            ;;
-        "--main-file" | "-mf")
-            shift; main_file="$1"
-            ;;
-        "--title" | "-t")
-            shift; title="$1"
-            ;;
-        "--dry-run" | "-dr")
-            dry_run="echo"
-            ;;
-        "--verbose" | "-v")
-            verbose=1
-            ;;
-        *)
-            echo "erreur: Argument non reconnu : $1"
-            usage
-            exit 1
-            ;;
-    esac
-    shift
-done
+parse_args () {
+    while test $# -ne 0; do
+	case "$1" in
+            "--help"|"-h")
+		help
+		exit 0
+		;;
+            "--source" | "--src" | "-s")
+		shift; src="${1%/}"
+		;;
+            "--destination" | "--dest" | "-d")
+		shift; dest="${1%/}"
+		;;
+            "--main-file" | "-mf")
+		shift; main_file="$1"
+		;;
+            "--title" | "-t")
+		shift; title="$1"
+		;;
+            "--dry-run" | "-dr")
+		dry_run="echo"
+		;;
+            "--verbose" | "-v")
+		verbose=1
+		;;
+            *)
+		echo "erreur: Argument non reconnu : $1"
+		usage
+		exit 1
+		;;
+	esac
+	shift
+    done
+}
 
-# Si la verbose est inactive, redirection de nos sorties vers /dev/null
-if [ "$verbose" = 1 ]; then
-    exec 4>&2 3>&1
-else
-    exec 4>/dev/null 3>/dev/null
-fi
+# Initialise les elements nécessaires au script en fonction 
+# des résultats du parsing
+init () {
+    # On doit avoir au minimum le repertoire source et dest comme arguments
+    if [ "$src" = "" -o "$dest" = "" ]; then
+	echo "erreur: Il manque un ou plusieurs arguments." >&2
+	usage; exit 1
+    fi
 
-# On doit avoir au minimum le repertoire source et dest comme arguments
-if [ "$src" = "" -o "$dest" = "" ]; then
-    echo "erreur: Il manque un ou plusieurs arguments."
-    usage; exit 1
-fi
+    # Notification d'activation du dry_run
+    if [ $dry_run ]; then 
+	printf "!-- Execution a sec, aucune commande ne sera executee --!\n\n" >&2
+    fi
 
-if [ $dry_run ]; then 
-    printf "!-- Execution a sec, aucune commande ne sera executee --!\n\n"
-fi
+    # Creation de l'arborescence du site si inexistante
+    if ! [ -d "$dest" ]; then
+	$dry_run mkdir "$dest"
+    fi
+    if ! [ -d "$dest/images/" ]; then
+	$dry_run mkdir "$dest/images/"
+    fi
+    if ! [ -d "$dest/vignettes/" ]; then
+	$dry_run mkdir "$dest/vignettes/"
+    fi
+    if ! [ -d "$dest/viewers/" ]; then
+	$dry_run mkdir "$dest/viewers/"
+    fi
 
-# Creation de l'arborescence du site si inexistante
-if ! [ -d "$dest" ]; then
-    $dry_run mkdir "$dest"
-fi
-if ! [ -d "$dest/images/" ]; then
-    $dry_run mkdir "$dest/images/"
-fi
-if ! [ -d "$dest/vignettes/" ]; then
-    $dry_run mkdir "$dest/vignettes/"
-fi
-if ! [ -d "$dest/viewers/" ]; then
-    $dry_run mkdir "$dest/viewers/"
-fi
+    # Vérification que exiftags est compilé
+    if ! [ -f "$DIR"/exiftags  ]; then
+	echo "./exiftags n'existe pas, on le compile: " >&2
+	(cd exiftags && make)
+    fi
 
-## Ecriture du fichier HTML
-# Ouverture du fichier de sortie sur le desc 5
-# et ecriture du header et titre
-if [ $dry_run ]; then 
-    echo 'exec 5>' "$dest/$main_file"
-    echo 'html_head '"$title"' >&5'
-    echo 'html_title '"$title"' >&5'
-else 
-    exec 5> "$dest/$main_file"
-    html_head "$title" >&5
-    html_title "$title" >&5
-fi
-
-# Generation des vignettes pour chaque image jpeg de src
-printf "\n* Generation des vignettes... \n"
-
-for file in "$src"/*; do
-    filename=$(basename "$file")
-
-    # Copie de l'image dans $dest/images/ si inexistante
-    if ! [ -f "$dest/images/$filename" ]; then
-	echo "cp $file $dest/images" >&3
-	$dry_run cp "$file" "$dest"/images/
+    ## Si la verbose est inactive, redirection de nos sorties vers /dev/null
+    # 1: stdout
+    # 2: stderr
+    # 3: verbose stdout
+    # 4: verbose stderr
+    # 5: fichier html
+    if [ $dry_run ]; then 
+	echo 'exec 5> $dest/$main_file'
+    else
+	exec 5> "$dest/$main_file"
     fi
     
-    # Si la vignette n'existe pas, la creer
-    if [ -f "$dest/vignettes/vg-$filename" ]; then
-        echo "$dest/vignettes/vg-$filename existe deja, on l'ignore..." >&3
-    else 
-	echo "convert -thumbnail 320x240 $file $dest/vignettes/vg-$filename" >&3
-	$dry_run convert -thumbnail 320x240 \
-	    "$file" "$dest/vignettes/vg-$filename"
-    fi
-done
-
-# Ecriture des images dans le fichier HTMl
-printf "\n* Ecriture des images dans le fichier HTMl...\n"
-
-$dry_run cd "$dest"
-
-while true; do
-    echo "Voulez vous entrer des legendes pour chaque image ? [oO | (nN)]"
-    read legendMode
-    case "$legendMode" in
-	"o" | "O" | "Oui")
-	    break
-	    ;;
-	"n" | "N" | "Non")
-	    break
-	    ;;
-	'')
-	    legendMode='n'; break
-	    ;;
-	*)
-	    echo "Veuillez repondre par oui ou non."
-	    ;;
-    esac
-done
-
-for img in images/*; do
-    imageName=$(basename "$img")
-
-    if ! [ $legendMode = 'n' -o $legendMode = 'N' ]; then
-	echo "--> Veuillez entrer la legende de l'image $imageName: "
-	$dry_run read legend
-    fi
-
-    printf "generate-img-fragment.sh vignettes/vg-%s images/%s " \
-     	"$imageName" "$imageName" >&3
-    printf "%s %s > %s \n" "$imageName" "$legend" "$main_file" >&3
-
-    viewerName=$(basename "$imageName" .jpg).html
-
-    if [ $dry_run ]; then 
-	echo "$DIR"/generate-img-fragment.sh vignettes/vg-"$imageName" \
-	    "$viewerName"' >&5'
-
-	echo "$DIR"'/generate-viewer.sh '"$imageName $legend $main_file" \
-	    '> viewers/'"$viewerName"
+    if [ "$verbose" = 1 ]; then
+	exec 4>&2 3>&1
     else
-	"$DIR"/generate-img-fragment.sh vignettes/vg-"$imageName" \
-	    "$viewerName" "$legend" >&5
-	"$DIR"/generate-viewer.sh "$imageName" "$legend" "$main_file" \
-	    > viewers/"$viewerName"
+	exec 4>/dev/null 3>/dev/null
     fi
-done
+}
 
-$dry_run cd "$DIR"
+print_index_header () {
+    # Ecriture du header et titre
+    if [ $dry_run ]; then 
+	echo 'html_head '"$title"' >&5'
+	echo 'html_title '"$title"' >&5'
+    else 
+	html_head "$title" >&5
+	html_title "$title" >&5
+    fi
+}
 
-if [ $dry_run ]; then echo 'html_tail >&5'; else html_tail >&5; fi
+# Generation des vignettes pour chaque image jpeg de src
+generate_thumbs () {
+    printf "* Generation des vignettes... \n"
+
+    for file in "$src"/*; do
+	filename=$(basename "$file")
+
+	# Copie de l'image dans $dest/images/ si inexistante
+	if ! [ -f "$dest/images/$filename" ]; then
+	    echo "cp $file $dest/images" >&3
+	    $dry_run cp "$file" "$dest"/images/
+	fi
+	
+	# Si la vignette n'existe pas, la creer
+	if [ -f "$dest/vignettes/vg-$filename" ]; then
+            echo "$dest/vignettes/vg-$filename existe deja, on l'ignore..." >&3
+	else 
+	    echo "convert -thumbnail 320x240 $file $dest/vignettes/vg-$filename" >&3
+	    $dry_run convert -thumbnail 320x240 \
+		"$file" "$dest/vignettes/vg-$filename"
+	fi
+    done
+}
+
+# Inclusion des images dans le fichier HTMl
+include_images () {
+    printf "\n* Ecriture des images dans le fichier HTMl...\n"
+    
+    $dry_run cd "$dest"
+
+    for img in images/*; do
+	imageName=$(basename "$img")
+	viewerName=$(basename "$imageName" .jpg).html
+
+	# Formatage de la date pour la légende
+	date=$(exif_date "$img")
+	if ! [ "$date" = "//"  ]; then
+	    legend="$(basename "$img" .jpg) – $date"
+	else
+	    legend="$(basename "$img" .jpg)"
+	fi
+
+	if [ "$dry_run" != '' -o "$verbose" -eq 1 ]; then 
+	    echo "$DIR"/generate-img-fragment.sh vignettes/vg-"$imageName" \
+		"$viewerName"' >&5'
+
+	    echo "$DIR"'/generate-viewer.sh '"$imageName $legend $main_file" \
+		'> viewers/'"$viewerName"
+	else
+	    "$DIR"/generate-img-fragment.sh vignettes/vg-"$imageName" \
+		"$viewerName" "$legend" >&5
+		"$DIR"/generate-viewer.sh "$imageName" "$legend" "$main_file" \
+		    > viewers/"$viewerName"
+	fi
+    done
+}
+
+print_index_footer () {
+    if [ $dry_run ]; then 
+	echo 'html_tail >&5'; 
+    else 
+	html_tail >&5; 
+    fi
+}
+
+echo "
+                 --------- Script Gallerie Shell ---------
+"
+
+parse_args $@
+init
+
+print_index_header
+
+generate_thumbs
+include_images
+
+print_index_footer
 
 printf "\n* Fini: Le fichier html genere est disponible ici: %s/%s \n" \
     "$dest" "$main_file"
+echo "
+                  -- Par Guillaume Halb & Pierre Thalamy --"
+
 exit 0
