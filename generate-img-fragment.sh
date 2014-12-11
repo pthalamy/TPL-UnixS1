2573931a5f39cd7538f33e447c7908e630d546be
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-12-11 20:06:56 +0100

    add alt and legend, ignore if file is not present

--- generate-img-fragment.sh
+++ generate-img-fragment.sh
@@ -4,20 +4,20 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR"/utilities.sh
 
 usage () {
-    echo 'usage: generate-img-fragment.sh vignette viewer' >&2
+    echo 'usage: generate-img-fragment.sh vignette viewer date' >&2
 }
 
 if [ $# -ne 1 ]; then
-    if ! [ -r "$1" ]; then
-	echo "generate-img-fragment.sh: Le fichier $1 n'existe pas." >&2
-	exit 1;
-    else
+	legend="$(basename "$2" .html)"
+
 	echo '<div class="vgframe">'
 	echo '<a href="'$2'" target="_top">'
-	echo '<img class="image" src="'$(FileRelative2Absolute "$1")'"><br>'
+	echo '<img class="image" '
+	echo 'alt="'"$(basename "$1" .jpg)"'" '
+	echo 'src="'"$(FileRelative2Absolute "$1")"'"><br>'
 	echo '</a>'
+	echo '<span class="vg-legend">'"$legend"'</span>'
 	echo '</div>'
-    fi
     
     exit 0
 else
